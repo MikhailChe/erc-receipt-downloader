81201d15e251591b8af326a55260b49ad99be3ed
Mikhail <mikhail-chernoskutov@gmail.com>
2021-03-17 22:57:06 +0500

    Logging

--- library/telegram/__init__.py
+++ library/telegram/__init__.py
@@ -12,14 +12,18 @@ def send_message(msg):
     if not (TELEGRAM_TOKEN and NOTIFICATION_CHAT_ID):
         log.warning('Tried to send message, but telegram is not configured')
         return
+    log.info('Sending telegram message %s', msg)
     bot = telebot.TeleBot(TELEGRAM_TOKEN)
     bot.send_message(NOTIFICATION_CHAT_ID, msg)
+    log.info('Done')
 
 
 def send_document(filename, caption):
     if not (TELEGRAM_TOKEN and NOTIFICATION_CHAT_ID):
         log.warning('Tried to send document, but telegram is not configured')
         return
+    log.info('Sending telegram file %s', filename)
     with open(filename, 'rb') as f:
         bot = telebot.TeleBot(TELEGRAM_TOKEN)
         bot.send_document(NOTIFICATION_CHAT_ID, f, caption=caption)
+    log.info('Done')
