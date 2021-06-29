cdbd86f1bb4f4cbba052af9df938970832bd4db2
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2021-06-29 10:38:18 +0500

    Update README and example config

--- config_ex.sh
+++ config_ex.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
+export LOG_PATH=logs
 export ERC_LOGIN=
 export ERC_PASSWORD=
 export ERC_CONTRACT_NUMBERS=
+export ERC_DATA_DIR=./data
 export TELEGRAM_TOKEN=
 export TELEGRAM_CHAT_ID=
